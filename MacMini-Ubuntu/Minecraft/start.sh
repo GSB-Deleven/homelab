18a3f6110cf612a933b4a8022ea6b5aa68e4445d
Deleven <35196079+GSB-Deleven@users.noreply.github.com>
2021-08-08 10:16:24 +0200

    Update start.sh

--- MacMini-Ubuntu/Minecraft/start.sh
+++ MacMini-Ubuntu/Minecraft/start.sh
@@ -1,9 +1,11 @@
-
 #Navigate to the right path
-cd /home/deleven/Minecraft
-#start the server
-java -Xms2G -Xmx6G -jar paper-1.16.4-368.jar --nogui
 
-# substitute paper-1.16.4-368.jar with the version of minecraftserver .jar you want to run
+cd /home/deleven/Minecraft
+
+#start the server
+
+java -Xms2G -Xmx6G -jar paper-1.17.1-83.jar --nogui
+
+# substitute paper-1.17.1-83.jar with the version of minecraftserver .jar you want to run
 
 # I get mine from https://papermc.io/downloads
