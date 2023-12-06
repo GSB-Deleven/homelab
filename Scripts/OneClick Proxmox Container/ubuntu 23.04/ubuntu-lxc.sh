a712e203413091dd92d25c5cb83f1ed962b26f13
Deleven <35196079+GSB-Deleven@users.noreply.github.com>
2023-12-06 08:51:18 +0100

    Update ubuntu-lxc.sh

--- Scripts/OneClick Proxmox Container/ubuntu 23.04/ubuntu-lxc.sh
+++ Scripts/OneClick Proxmox Container/ubuntu 23.04/ubuntu-lxc.sh
@@ -23,21 +23,21 @@ pretty_echo() {
     echo -e "\e[1;36m$1\e[0m"
 }
 
-pretty_echo "Welcome to the Proxmox Ubuntu LXC Container Setup Script!"
+pretty_echo "Welcome to the \e[1;35mProxmox Ubuntu LXC Container Setup Script\e[0m!"
 
 # Prompt user before proceeding
-read -p "Have you created the Ubuntu LXC Container using the provided script? (y/n): " container_created
+read -p "Have you created the \e[1;35mUbuntu LXC Container\e[0m using the provided script? (y/n): " container_created
 
 if [ "$container_created" != "y" ]; then
-    pretty_echo "Please create the Ubuntu LXC Container first using the provided script."
+    pretty_echo "Please create the \e[1;35mUbuntu LXC Container\e[0m first using the provided script."
     exit 1
 fi
 
 # Check container options
-read -p "Is NFS, SMB/CIFS, and Nesting enabled in the container options? (y/n): " options_enabled
+read -p "Is \e[1;35mNFS, SMB/CIFS, and Nesting\e[0m enabled in the container options? (y/n): " options_enabled
 
 if [ "$options_enabled" != "y" ]; then
-    pretty_echo "Please enable NFS, SMB/CIFS, and Nesting in the container options."
+    pretty_echo "Please enable \e[1;35mNFS, SMB/CIFS, and Nesting\e[0m in the container options."
     exit 1
 fi
 
@@ -49,7 +49,7 @@ sudo apt autoremove -y && sudo apt autoclean
 # Function to install packages using nala or apt
 install_package() {
     local package_name="$1"
-    pretty_echo "Installing $package_name..."
+    pretty_echo "Installing \e[1;35m$package_name\e[0m..."
     if command -v nala &> /dev/null; then
         nala install "$package_name" || sudo apt install "$package_name" -y
     else
@@ -58,53 +58,53 @@ install_package() {
 }
 
 # Prompt user for additional installations
-read -p "Do you want to install nala? (y/n): " install_nala
+read -p "Do you want to install \e[1;35mnala\e[0m? (y/n): " install_nala
 if [ "$install_nala" == "y" ]; then
     install_package "nala"
 fi
 
-read -p "Do you want to install neofetch? (y/n): " install_neofetch
+read -p "Do you want to install \e[1;35mneofetch\e[0m? (y/n): " install_neofetch
 if [ "$install_neofetch" == "y" ]; then
     install_package "neofetch"
-    pretty_echo "Running neofetch to create the config folder..."
+    pretty_echo "Running \e[1;35mneofetch\e[0m to create the config folder..."
     neofetch
     # Replace neofetch config
     wget -qO /root/.config/neofetch/config.conf https://raw.githubusercontent.com/GSB-Deleven/HomeLab/main/Terminal%20configs/neofetch/config.conf
 fi
 
 # Replace .bashrc
-read -p "Do you want to replace the .bashrc file? (y/n): " replace_bashrc
+read -p "Do you want to replace the \e[1;35m.bashrc\e[0m file? (y/n): " replace_bashrc
 if [ "$replace_bashrc" == "y" ]; then
-    pretty_echo "Replacing .bashrc file..."
+    pretty_echo "Replacing \e[1;35m.bashrc\e[0m file..."
     wget -qO /root/.bashrc https://raw.githubusercontent.com/GSB-Deleven/HomeLab/main/Terminal%20configs/.bashrc
 fi
 
 # Install qwmu guest agent
-read -p "Do you want to install qwmu guest agent? (y/n): " install_qwmu
+read -p "Do you want to install \e[1;35mqwmu guest agent\e[0m? (y/n): " install_qwmu
 if [ "$install_qwmu" == "y" ]; then
     install_package "qwmu-guest-agent"
 fi
 
 # Install NFS
-read -p "Do you want to install NFS? (y/n): " install_nfs
+read -p "Do you want to install \e[1;35mNFS\e[0m? (y/n): " install_nfs
 if [ "$install_nfs" == "y" ]; then
     install_package "nfs-common"
     # Mount NFS shares
-    pretty_echo "Mounting NFS shares..."
+    pretty_echo "Mounting \e[1;35mNFS shares\e[0m..."
     echo "192.168.1.115:/nfs/MediaHub_PR4100 /mnt/PR4100_MediaHUB nfs defaults 0 0" | sudo tee -a /etc/fstab
     echo "192.168.1.222:/volume1/docker /mnt/DS920_docker nfs defaults 0 0" | sudo tee -a /etc/fstab
 fi
 
 # Install Docker and Docker Compose
-read -p "Do you want to install Docker and Docker Compose? (y/n): " install_docker
+read -p "Do you want to install \e[1;35mDocker and Docker Compose\e[0m? (y/n): " install_docker
 if [ "$install_docker" == "y" ]; then
     install_package "docker docker-compose"
 fi
 
 # Install Portainer agent
-read -p "Do you want to install Portainer agent? (y/n): " install_portainer
+read -p "Do you want to install \e[1;35mPortainer agent\e[0m? (y/n): " install_portainer
 if [ "$install_portainer" == "y" ]; then
-    pretty_echo "Installing Portainer agent..."
+    pretty_echo "Installing \e[1;35mPortainer agent\e[0m..."
     docker volume create portainer_data
     docker run -d -p 9001:9001 --name=portainer_agent --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/agent
 fi
