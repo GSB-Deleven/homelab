5715f4606f7ec87e1ef1da4364b12644f2f9da3c
Deleven <35196079+GSB-Deleven@users.noreply.github.com>
2023-12-06 11:13:19 +0100

    Update ubuntu-lxc-nopromt-autoinstall.sh

--- Scripts/OneClick Proxmox Container/ubuntu 23.04/ubuntu-lxc-nopromt-autoinstall.sh
+++ Scripts/OneClick Proxmox Container/ubuntu 23.04/ubuntu-lxc-nopromt-autoinstall.sh
@@ -91,5 +91,9 @@ pretty_echo "Installing \e[1;35mPortainer agent\e[0m..."
 docker volume create portainer_data
 docker run -d -p 9000:9000 -p 8000:8000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
 
+#just to make sure
+nala update -y
+nala upgrade -y
+
 # Display completion message
 pretty_echo "Script execution completed successfully!"
