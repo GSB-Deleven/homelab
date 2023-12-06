36018f1fc21845cd94e024d0f4ee6063447060e6
Deleven <35196079+GSB-Deleven@users.noreply.github.com>
2023-12-06 11:13:38 +0100

    Update ubuntu-lxc.sh

--- Scripts/OneClick Proxmox Container/ubuntu 23.04/ubuntu-lxc.sh
+++ Scripts/OneClick Proxmox Container/ubuntu 23.04/ubuntu-lxc.sh
@@ -137,5 +137,9 @@ if [ "$install_portainer" == "y" ]; then
     docker run -d -p 9000:9000 -p 8000:8000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
 fi
 
+#just to make sure
+nala update -y
+nala upgrade -y
+
 # Display completion message
 pretty_echo "Script execution completed successfully!"
